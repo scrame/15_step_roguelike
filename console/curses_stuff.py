41699542ba587d8b53ac1e8eeaf12487f41b4548
liam christopher <liam.christopher@cbs.com>
2010-08-17 13:29:12 -0700

    really basic controls.

--- console/curses_stuff.py
+++ console/curses_stuff.py
@@ -4,10 +4,34 @@ import curses
 import curses.ascii
 from curses import panel
 
+
+x_max = 50
+y_max = 50
+
+x = 20
+y = 20
+
+
+def update_pos(c):
+    global x,y
+    if(259 == c):
+        x = (x_max if x==0 else x-1)
+    if(258 == c):
+        x = (0 if x==x_max else x+1)
+
+    if(260 == c):
+        y = (y_max if y==0 else y-1)
+    if(261 == c):
+        y = (0 if y==y_max else y+1)
+
+
 def run(win):
+    
     while(1):
-        win.move(20,20)
-        win.addstr(curses.ascii.unctrl(win.getch()))
+        update_pos(win.getch())
+        win.move(x,y)
+        win.addstr('#')
+        win.move(0,0)
         win.refresh() 
 
 curses.wrapper(run)
